c1602a2dfdc21bbe8cd515b76d3cea0ddfd7168b
mediaptera <mediaptera@231ce8e5-bf24-f191-bd12-36d47a45c5de>
2010-08-21 19:45:53 +0000

    GAE framework complete

--- trunk/gae/main.py
+++ trunk/gae/main.py
@@ -17,22 +17,48 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
+try:
+    import json
+except ImportError:
+    from django.utils import simplejson as json
 
 class Company(db.Model):
     name = db.StringProperty(required=True)
     duns = db.IntegerProperty(required=True)
-    awards = db.ListProperty(required=True)
+    awards = db.ListProperty(str,required=True)
     jobs = db.IntegerProperty(required=True)
     totalReceived = db.IntegerProperty(required=True)
+    
+    def __str__(self):
+        data = {"NAME" : self.name,
+                "DUNS" : self.duns,
+                "AWARDS": map(lambda id: lookupAward(id), self.awards),
+                "JOBS" : self.jobs,
+                "RCVD" : self.totalReceived}
+        return json.dumps(data)
 
 class Award(db.Model):
     lat = db.FloatProperty()
-    long = db.FloatProperty()
+    lon = db.FloatProperty()
     id = db.StringProperty(required=True)
     amount = db.IntegerProperty(required=True)
     funding_agency = db.StringProperty(required=True)
     recipient_duns = db.IntegerProperty(required=True)
     description = db.TextProperty()
+    
+    def __str__(self):
+        data = {"LAT" : lat,
+                "LON" : lon,
+                "ID" : id,
+                "AMT" : amount,
+                "FUNDING_AGCY" : funding_agency,
+                "RECIP_DUNS" : repient_duns,
+                "DESC" : description}
+        return json.dumps(data)
+
+def lookupAward(id):
+    award = Award.gql("WHERE id = :1", id)
+    return str(award)
 
 class AdminHandler(webapp.RequestHandler):
     def get(self):
@@ -41,19 +67,31 @@ class AdminHandler(webapp.RequestHandler):
 <body>
 <form action="/admin" method="post">
 <input type="text" name="name">
-
 <input type="text" name="duns">
+<input type="text" name="awards">
+<input type="text" name="jobs">
+<input type="text" name="received">
 <input type="submit" value="Enter Company">
 </body>
 </html>
-"""
+""")
+    def post(self):
+        name = self.request.get("name")
+        duns = long(self.request.get("duns"))
+        awards = map(lambda x: x.strip(), self.request.get("awards").split(","))
+        jobs = int(self.request.get("jobs"))
+        recvd = long(self.request.get("received"))
+        c = Company(name=name, duns=duns, awards=awards, jobs=jobs, totalReceived=recvd)
+        self.response.out.write(c)
+        c.put()
+        
 
 class DetailHandler(webapp.RequestHandler):
     def get(self):
-        #company_id = self.request.get("company")
-        #matches = Company.gql("WHERE duns = :1", company_id)
-        pass
-
+        company_id = long(self.request.get("company"))
+        matches = Company.gql("WHERE duns = :1", company_id)
+        self.response.out.write(matches[0])
+        
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
