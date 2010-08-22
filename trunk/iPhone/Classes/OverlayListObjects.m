d7fb991a49b7a26f802a3f540ac570094930bd4c
danthac <danthac@231ce8e5-bf24-f191-bd12-36d47a45c5de>
2010-08-22 01:09:35 +0000

    Added mapView overlay files

---
+++ trunk/iPhone/Classes/OverlayListObjects.m
@@ -0,0 +1,16 @@
+//
+//  OverlayListObjects.m
+//  iRecoveryWatch
+//
+//  Created by Dantha Manikka-Baduge on 8/21/10.
+//  Copyright 2010 dandil. All rights reserved.
+//
+
+#import "OverlayListObjects.h"
+
+
+@implementation OverlayListObjects
+
+@synthesize boundingMapRect, coordinate;
+
+@end
